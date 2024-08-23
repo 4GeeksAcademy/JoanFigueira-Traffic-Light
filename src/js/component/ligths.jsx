4a4e1f765879ad56aba75e2d65d8c20022e6dceb
Joan Figueira <joanfigueira365@gmail.com>
2024-08-23 19:22:13 +0000

    feat Traffic Light

--- src/js/component/ligths.jsx
+++ src/js/component/ligths.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 
-export const TrafficLights= () => {
-    // Code JS
-    const [color,setColor] = useState('glow')
-    const [background,setBackgrond] = useState ('secondary')
+export const TrafficLights = () => {
+	// Code JS
+	const [color, setColor] = useState('glow');
+	const [background, setBackgrond] = useState('');
 
-
-
-    return (
-        <>
-			<div className="base"></div>
-				<div className="text-center container">
-					<div className="traffic-lights">
-						<div onClick={() => setColor('red')} className={'light-red' + (color === 'red' ? ' glow' : '')}></div>
-						<div onClick={() => setColor('yellow')} className={'light-yellow' + (color === 'yellow' ? ' glow' : '')}></div>
-						<div onClick={() => setColor('green')} className={'light-green' + (color === 'green' ? ' glow' : '')}></div>
+	return (
+		<>
+			<div className="base">
+			</div>
+			<div className="text-center container">
+				<div className="traffic-lights">
+					<div onClick={() => setColor('red')} 
+					className={(background === ' purple' ? ' purple ' : 'light-red') + (color === 'red' ? ' glow' : '') + (background === ' purple' ? 'light-red' : '')}></div>
+					<div onClick={() => setColor('yellow')} 
+					className={(background === ' purple' ? ' purple ' : 'light-yellow') + (color === 'yellow' ? ' glow' : '')}></div>
+					<div onClick={() => setColor('green')} 
+					className={(background === ' purple' ? ' purple ' : 'light-green') + (color === 'green' ? ' glow' : '')}></div>
 				</div>
-			</div> 
+			</div>
+			<div className="container text-center">
+				<button onClick={() => setBackgrond(' purple')} type="button" className="btn btn-primary m-1">Change Color</button>
+				<button onClick={() => setBackgrond('primary')} type="button" className="btn btn-info m-1">Reset</button>
+			</div>
+
 		</>
-        
-    )
+	)
 }
